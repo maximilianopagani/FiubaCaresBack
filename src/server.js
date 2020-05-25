2f7e76191cbf32cd85be262d107555ee9358ce4c
Ariel Ruiz <ariel.ruiz@mercadolibre.com>
2020-05-25 19:04:10 -0300

    Removing duplicate var.-

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const app = require('./app');
-const port = process.env.PORT;
-const dbName = "FiubaCMS";
-
 const portToUse = process.env.PORT || '3000';
 
 app.listen(portToUse, () => {
