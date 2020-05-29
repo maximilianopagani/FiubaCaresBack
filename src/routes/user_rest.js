e2bf4d5acb2fc7f658a8143825756dbd44d98868
Ariel Ruiz <ariel.ruiz@mercadolibre.com>
2020-05-29 02:02:44 -0300

    Adding cors for everyone.-

--- src/routes/user_rest.js
+++ src/routes/user_rest.js
@@ -1,20 +1,19 @@
 'use strict';
 const express = require('express');
-const cors = require('cors');
 const UserController = require('./../controllers/user');
 
 let api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
-const corsOptions = {
+/*const corsOptions = {
     origin: 'http://fiubacares.herokuapp.com/',
     optionsSuccessStatus: 200
-};
+};*/
 
 // Creamos una ruta para los métodos que tenemos en nuestros controladores
-api.get('/user/:username/:password', cors(corsOptions), md_auth.ensureAuth, UserController.getUser);
-api.get('/users', cors(corsOptions), md_auth.ensureAuth, UserController.getAll);
-api.get('/', cors(corsOptions), (req, res) => {
+api.get('/user/:username/:password', md_auth.ensureAuth, UserController.getUser);
+api.get('/users', md_auth.ensureAuth, UserController.getAll);
+api.get('/', (req, res) => {
     res.send('acp1-2020 api');
 });
 
